9d32b11f462c87b97838ec244d914e642b4d7f11
Hudson <hud.lucasgreg@gmail.com>
2026-02-11 21:06:55 -0300

    Primeiro comit

---
+++ script.js
@@ -0,0 +1,22 @@
+// comentario
+/* comentario de bloco */
+
+// coisas gostosas
+
+//  Var não é muito seguro, não usar muito
+//  Let: usa quando tiver que reatibuir
+//  Const: Usa quando não precisa reatribuir
+var cidade = "registro";
+var cidade = "sete barras";
+document.write(cidade);
+
+let nome = maria;
+nome = diego
+document.write("<br>")
+document.write(nome)
+
+const pais = "brasil"
+pais = "argentina"
+
+
+document.write(pais)
